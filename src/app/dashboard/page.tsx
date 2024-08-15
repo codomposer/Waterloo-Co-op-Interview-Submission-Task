b19d7c0c911792bf7b42e0a405962f7f39fe6fcf
codomposer <akileus0902@gmail.com>
2024-08-15 16:57:22 -0400

    chore: removed unused variable

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,10 +1,6 @@
 "use client";
 
 import React from "react";
-import Image from "next/image";
-import Link from "next/link";
-import { motion, useAnimation } from "framer-motion";
-import { useInView } from "react-intersection-observer";
 
 import { Button } from "@/components/button/Button";
 import Products from "./components/Products";
